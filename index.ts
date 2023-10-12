72c9ff8153f94ab51da79c842448bfd6882b9121
Batool Shaheen <batoulshaheen11@gmail.com>
2023-10-12 19:16:40 +0300

    add routers

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import './config.js';
 import express from "express";
 import { initDB } from "./DB/dataSource.js";
+import usersRouter from './routes/userRouter.js';
+import orderRouter from './routes/orderRouter.js'
+import productRoiter from './routes/productrouter.js';
+import orderItemRouter from './routes/orderItemRouter.js';
 
 
 const app = express();
@@ -8,6 +12,13 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
+app.use('/users', usersRouter);
+app.use('/product', productRoiter);
+app.use('/orderItem', orderItemRouter);
+app.use('/orders', orderRouter);
+
+
+
 
 
 
