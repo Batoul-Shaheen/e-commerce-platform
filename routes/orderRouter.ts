9ed418f4fa9b3fc3c5e8009d55a6101c91d745b3
Batool Shaheen <batoulshaheen11@gmail.com>
2023-10-27 17:53:41 +0300

    deployment

--- routes/orderRouter.ts
+++ routes/orderRouter.ts
@@ -56,7 +56,6 @@ router.post("/add-to-order/product/:productId/order/:orderId", async (req, res) 
       order.orderItem.push(orderItem);
     }
 
-    // Calculate the total amount for the order
     order.totalAmount = order.orderItem.reduce((total, oi) => total + oi.products.price * oi.quantity, 0);
 
     await orderItem.save();
@@ -98,95 +97,6 @@ router.get("/order/:id", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
-
-// router.post("/add-to-order/product/:productId/order/:orderId",// authorize('POST-PTO'),
-//  async (req, res) => {
-//   try {
-//     const order = await Order.findOne({
-//       relations: ["orderItem.products","products"],
-//       where: { id: parseInt(req.params.orderId) },
-//     });
-//     const product = await Product.findOne({
-//       where: { id: parseInt(req.params.productId) },
-//     });
-
-//     if (!order || !product) {
-//       return res.status(404).send("Order or product not found");
-//     }
-
-//     const existingProductCart = order.orderItem? order.orderItem.find(
-//       (orderItems) => orderItems.products.id === parseInt(req.params.productId)
-//     )
-//     :undefined;
-
-//     if (existingProductCart) {
-//       existingProductCart.quantity += 1;
-//       order.totalAmount += (existingProductCart.products.price * existingProductCart.quantity);
-//       await existingProductCart.save();
-//     } else {
-//       const newOrderItem = new OrderItem();
-//       newOrderItem.products = product;
-//       newOrderItem.quantity = 1;
-//       order.totalAmount += (newOrderItem.products.price * newOrderItem.quantity);
-
-//       if (!order.orderItem) {
-//         order.orderItem = [];
-//       }
-
-//     order.orderItem.push(newOrderItem);
-//     await newOrderItem.save();
-//     await order.save();
-//   } 
-
-//     return res.status(200).send("Product added to the order");
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).send("Internal server error");
-//   }
-// });
-
-
-// router.get("/:userId/:orderId",// authorize('GET-order'),
-//  async (req, res) => {
-//   try {
-//     const userId = req.params.userId;
-//     const orderId = parseInt(req.params.orderId)
-
-//     const user = await User.find({ where: { id: parseInt(userId) } });
-
-//     if (!user) {
-//       return res.status(404).send("User not found");
-//     }
-
-//     const orders = await Order.find({
-//       where: { id: parseInt(userId) },
-//     });
-
-//     const order = await Order.findOne({ relations: ['products'], where: { id: orderId } });
-//     if (!order) {
-//       return res.status(404).json({ message: 'Order not found' });
-//     }
-
-//     const productDetails = order.products.map((product) => {
-//       return {
-//         id: product.id,
-//         name: product.name,
-//         description: product.description,
-//         price: product.price,
-//         quantity: product.quantity,
-//       };
-//     });
-
-//     res.status(200).send({
-//       user: user,
-//       orders: orders,
-//       Product: productDetails,
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Internal server error");
-//   }
-// });
 
 router.put("/:orderId", authorize('PUT-order'), async (req, res) => {
   const orderId = parseInt(req.params.orderId);
