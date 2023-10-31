ae7c4a251bcd0f425e33fdadc809e9144c9bba38
anwarmaswadeh <anwarmaswadeh77@gmail.com>
2023-10-31 02:16:04 +0200

    edit AWS SES

--- review.ts
+++ review.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 import aws from 'aws-sdk';
 import { User } from "./DB/entities/User.entity.js";
 
-const SESCONFIG ={
+const SESCONFIG = {
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_SES_REGION
@@ -14,15 +14,15 @@ const SESCONFIG ={
 
 const ses = new aws.SES(SESCONFIG);
 
-router.post('/email', async(req, res) => {
-    const {email, message, name} = req.body;
+router.post('/email', async (req, res) => {
+    const { email, review, name } = req.body;
     try {
-        const user = await User.findOneBy({ email : email });
+        const user = await User.findOneBy({ email: email });
         if (user) {
-            sesTest("platformecommerce54@gmail.com", email, message, name);
-            res.json({ message: 'Email sent successfully'}); 
-        }else {
-            res.status(400).json({ error: 'Email not found in the database' })
+            sesTest("platformecommerce54@gmail.com", email, review, name);
+            res.json({ email, review, name });
+        } else {
+            res.status(400).json({ error: 'Email not found, Please make sure you are signed up!' });
         }
     } catch (error) {
         console.log('Email sending error:', error);
@@ -30,25 +30,25 @@ router.post('/email', async(req, res) => {
     }
 });
 
-async function sesTest(emailTo: string, emailFrom: string, message: string, name: string) {
-        const params = {
-            Destination: {
-                ToAddresses: [emailTo],
-            },
-            Message: {
-                Body: {
-                    Text: {
-                        Data: `From User: ${name} \n ${message}`,
-                    },
-                },
-                Subject: {
-                    Data: `Name: ${emailFrom}`,
+async function sesTest(emailTo: string, emailFrom: string, review: string, name: string) {
+    const params = {
+        Destination: {
+            ToAddresses: [emailTo],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: `From User: ${name} \n ${review}`,
                 },
             },
-            Source: "platformecommerce54@gmail.com"
-        };
-        const res = await ses.sendEmail(params).promise();
-        
-} 
+            Subject: {
+                Data: `Name: ${emailFrom}`,
+            },
+        },
+        Source: "platformecommerce54@gmail.com"
+    };
+    const res = await ses.sendEmail(params).promise();
+
+}
 
 export default router;
